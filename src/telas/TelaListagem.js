bbdf91b5e46953f306e503edd078edeab5ff9ff7
1dois2 <breno.wajsfeld@ort.org.br>
2026-06-22 22:16:26 -0300

    commit 5, TelaListagem com FlatList e funcao de removeritem

---
+++ src/telas/TelaListagem.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
+
+export default function TelaListagem({ lista, removerItem }) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.titulo}>Seu Arsenal de Treino</Text>
+      
+      <FlatList 
+        data={lista}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => (
+          <View style={styles.itemContainer}>
+            <View style={styles.textoContainer}>
+              <Text style={styles.nome}>{item.nome} ({item.musculo})</Text>
+              <Text style={styles.detalhes}>{item.series} séries de {item.repeticoes}</Text>
+            </View>
+            <Button title='Remover' color='red' onPress={() => removerItem(item.id)} />
+          </View>
+        )}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1, padding: 20 },
+  titulo: { fontSize: 24, fontWeight: 'bold', marginBottom: 20, textAlign: 'center', marginTop: 30 },
+  itemContainer: { flexDirection: 'row', backgroundColor: '#fff', padding: 15, marginBottom: 10, borderRadius: 5, borderWidth: 1, borderColor: '#ddd', alignItems: 'center' },
+  textoContainer: { flex: 1 },
+  nome: { fontSize: 18, fontWeight: 'bold' },
+  detalhes: { fontSize: 14, color: '#666' }
+});
